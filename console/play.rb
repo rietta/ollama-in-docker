cbf568d386d0186f179455838a102fecb929ab80
Frank Rietta <frank@rietta.com>
2026-04-08 18:30:25 -0400

    Tweaks

--- console/play.rb
+++ console/play.rb
@@ -5,11 +5,14 @@ require "./core/ai_base.rb"
 
 puts "Have Fun: You have an object called ai, which is a class that can be used to interact with Ollama."
 
-ai = AiBase.new()
+@ai = AiBase.new()
 
+def chat(message, temperature = 0.7)
+    @ai.chat(message, temperature)
+    puts @ai.response_text
+end
 
-ai.chat("Hello, World. Please hold for a call from the programmer.")
-
+chat("Hello, World. Please hold for a call from the programmer.")
 
 binding.pry
 
